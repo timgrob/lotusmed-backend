725cbe25c211c48503cbb42c1cfa5345ef0e4146
Tim Grob <tim.grob@swissonline.ch>
2026-05-03 20:10:50 +0200

    makde endpoints async

--- src/api/v1/routes/users.py
+++ src/api/v1/routes/users.py
@@ -11,14 +11,14 @@ USERS_DB: dict[UUID, User] = {}
 
 
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
-def create_user(payload: UserCreate) -> User:
+async def create_user(payload: UserCreate) -> User:
     user = User(**payload.model_dump())
     USERS_DB[user.id] = user
     return user
 
 
 @router.put("/{user_id}", response_model=User)
-def update_user(user_id: UUID, payload: UserUpdate) -> User:
+async def update_user(user_id: UUID, payload: UserUpdate) -> User:
     if not (current_user := USERS_DB.get(user_id)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
@@ -30,7 +30,7 @@ def update_user(user_id: UUID, payload: UserUpdate) -> User:
 
 
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(user_id: UUID) -> None:
+async def delete_user(user_id: UUID) -> None:
     if user_id not in USERS_DB:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
