5f6ff13a98c3ab63de5f053188e7d2065b05a1fc
Tim Grob <tim.grob@swissonline.ch>
2026-07-07 00:25:58 +0200

    keep all models sepearate

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -6,28 +6,27 @@ from pydantic import BaseModel, ConfigDict, EmailStr, SecretStr
 from src.schemas.user_option import UserRole, UserStatus
 
 
-class UserBase(BaseModel):
+class UserCreate(BaseModel):
     username: str
     email: EmailStr
     role: UserRole = UserRole.USER
     status: UserStatus = UserStatus.PENDING
-
-
-class UserCreate(UserBase):
     password: SecretStr
 
 
 class UserUpdate(BaseModel):
     username: str | None = None
     email: EmailStr | None = None
-    password: SecretStr | None = None
     role: UserRole | None = None
     status: UserStatus | None = None
+    password: SecretStr | None = None
 
 
-class User(UserBase):
-    model_config = ConfigDict(from_attributes=True)
-
+class User(BaseModel):
     id: UUID
+    username: str
+    email: EmailStr
+    role: UserRole
+    status: UserStatus
     created_at: datetime
     updated_at: datetime
