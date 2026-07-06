026cd5bd093ebbd6f8469eef3286a73edff20055
Tim Grob <tim.grob@swissonline.ch>
2026-07-07 00:01:06 +0200

    fix types in settings file

--- src/core/config.py
+++ src/core/config.py
@@ -10,16 +10,22 @@ class Settings(BaseSettings):
     APP_DESCRIPTION: str = "Backend for the Lotusmed application"
     APP_VERSION: str = "0.1.0"
     APP_DEBUG: bool = False
+
     APP_PORT: int = 8000
     APP_HOST: str = "0.0.0.0"
     APP_RELOAD: bool = False
-    APP_API_VERSION: str = "/v1"
+    APP_API_VERSION: str = "v1"
+
+    APP_ENV: str = "dev"
+
+    DATABASE_URL: str = "sqlite:///database.db"
 
     OPENAI_API_KEY: SecretStr
-    OPENAI_CHATGPT_MODEL_VERSION: str = "gpt-5.5"
-    OPENAI_IMAGE_MODEL_VERSION: str = "gpt-image-2"
-    ANTROPIC_API_KEY: SecretStr
-    ANTROPIC_MODEL_VERSION: str = "opus-4.7"
+    OPENAI_MODEL_VERSION: str = "gpt-5.5"
+    ANTHROPIC_API_KEY: SecretStr
+    ANTHROPIC_MODEL_VERSION: str = "opus-4.7"
+    GEMINI_API_KEY: SecretStr
+    GEMINI_MODEL_VERSIOIN: str = ""
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent.parent / ".env",
